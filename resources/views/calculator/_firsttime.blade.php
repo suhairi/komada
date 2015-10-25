9d2af877df1041d2bc0a290eb866efee0569a2cf
suhairi81 <suhairi81@gmail.com>
2015-10-25 23:02:24 +0800

    2015-10-25 6

--- resources/views/calculator/_firsttime.blade.php
+++ resources/views/calculator/_firsttime.blade.php
@@ -3,50 +3,49 @@
     <div class="panel-body">
 
         {!! Form::open() !!}
-        <table class="table table-condensed">
+        <table class="table table-condensed table-hover">
             <tr>
                 <th>Jenis Pinjaman</th>
                 <td>Pinjaman Wang Tunai</td>
             </tr>
             <tr>
-                <th>Jumlah</th>
-                <td>{!! Form::number('jumlah', '0.00', ['class' => 'form-control', 'step' => 'any']) !!}</td>
+                <th>Jumlah Pinjaman (RM)</th>
+                <td>{!! Form::number('jumlah', '0.00', ['class' => 'form-control', 'step' => 'any', 'id' => 'jumlah', 'onkeyup' => 'calc()']) !!}</td>
             </tr>
             <tr>
                 <th>Tempoh</th>
-                <td>{!! Form::select('Tempoh', ['12', '24', '36', '48', '60'], null, ['class' => 'form-control', 'placeholder' => 'Kadar']) !!}</td>
+                <td>{!! Form::select('Tempoh', ['12' => '12', '24' => '24', '36' => '36', '48' => '48', '60' => '60'], null, ['class' => 'form-control', 'id' => 'tempoh', 'placeholder' => 'Kadar', 'onchange' => 'calc()']) !!}</td>
             </tr>
             <tr>
                 <th>Kadar (% setahun)</th>
-                <td>{!! Form::number('Kadar', 6, ['class' => 'form-control', 'readonly' => true]) !!}</td>
+                <td>{!! Form::number('Kadar', 6, ['class' => 'form-control', 'id' => 'kadar_peratus', 'readonly' => true]) !!}</td>
             </tr>
+
             <tr>
-                <th>Baki Tempoh</th>
-                <td>{!! Form::number('Baki Tempoh', 0, ['class' => 'form-control', 'readonly' => true]) !!}</td>
+                <th>Kadar Bulanan (RM)</th>
+                <td>{!! Form::number('Kadar', '0.00', ['class' => 'form-control', 'id' => 'kadar_bulanan', 'readonly' => true]) !!}</td>
             </tr>
+
             <tr>
-                <th>Jumlah Telah Bayar</th>
-                <td>{!! Form::number('jumlah_telah_bayar', '0.00', ['class' => 'form-control', 'readonly' => true, 'onkeyup' => 'calc()']) !!}</td>
+                <th>Kadar Bulanan * Tempoh</th>
+                <td>{!! Form::number('jumlah_kadar_keseluruhan', '0.00', ['class' => 'form-control', 'id' => 'jumlah_kadar_keseluruhan', 'readonly' => true]) !!}</td>
             </tr>
-            <tr>
-                <th>Baki</th>
-                <td>{!! Form::number('baki', '0.00', ['class' => 'form-control', 'readonly' => true]) !!}</td>
-            </tr>
+
             <tr>
                 <th>Bayaran Perkhidmatan</th>
-                <td>{!! Form::number('byrn_perkhidmatan', '0.00', ['class' => 'form-control', 'readonly' => true]) !!}</td>
+                <td>{!! Form::number('byrn_perkhidmatan', '0.00', ['class' => 'form-control', 'id' => 'byrn_perkhidmatan', 'readonly' => true]) !!}</td>
             </tr>
             <tr>
                 <th>Insurans</th>
-                <td>{!! Form::number('insurans', '0.00', ['class' => 'form-control', 'readonly' => true]) !!}</td>
+                <td>{!! Form::number('insurans', '0.00', ['class' => 'form-control', 'id' => 'insurans', 'readonly' => true]) !!}</td>
             </tr>
             <tr>
                 <th>Jumlah yang Perlu Dibayar</th>
-                <td>{!! Form::number('jumlah_perlu_bayar', '0.00', ['class' => 'form-control', 'readonly' => true]) !!}</td>
+                <td>{!! Form::number('jumlah_perlu_bayar', '0.00', ['class' => 'form-control', 'readonly' => true, 'id' => 'jumlah_perlu_bayar']) !!}</td>
             </tr>
             <tr>
                 <th>Bayaran Bulanan</th>
-                <td>{!! Form::number('bayaran_bulanan', '0.00', ['class' => 'form-control', 'readonly' => true]) !!}</td>
+                <td>{!! Form::number('byrn_bulanan', '0.00', ['class' => 'form-control', 'id' => 'byrn_bulanan', 'readonly' => true]) !!}</td>
             </tr>
 
         </table>
@@ -59,6 +58,25 @@
 
     function calc()
     {
-        alert('here');
+        jumlah = document.getElementById('jumlah').value;
+        kadar_peratus = document.getElementById('kadar_peratus').value
+        tempoh = document.getElementById('tempoh').value
+        insurans = document.getElementById('insurans').value
+
+        if(jumlah >= 1000)
+            document.getElementById('byrn_perkhidmatan').value = '50.00';
+        else
+            document.getElementById('byrn_perkhidmatan').value = '0.00';
+
+        document.getElementById('kadar_bulanan').value = kadar_peratus * jumlah / 100 / 12;
+
+        document.getElementById('jumlah_kadar_keseluruhan').value = document.getElementById('kadar_bulanan').value * tempoh;
+        kadar_keseluruhan = document.getElementById('jumlah_kadar_keseluruhan').value;
+
+
+        document.getElementById('jumlah_perlu_bayar').value = parseFloat(jumlah) + parseFloat(document.getElementById('byrn_perkhidmatan').value)
+        + (tempoh * document.getElementById('kadar_bulanan').value);
+
+        document.getElementById('byrn_bulanan').value = parseFloat(document.getElementById('jumlah_perlu_bayar').value) / parseInt(tempoh);
     }
 </script>
