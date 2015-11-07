1bfb6ebcb32e88299ca6bc99bd018e523e50aec3
suhairi81 <suhairi81@gmail.com>
2015-11-07 19:13:56 +0800

    2015-11-07 3

--- resources/views/members/laporan/potonganGaji.blade.php
+++ resources/views/members/laporan/potonganGaji.blade.php
@@ -4,24 +4,30 @@
 
     <div class="row">
         <div class="col-xs-12">
-            <div class="panel panel-heading"><h4>Jadual Potongan Gaji Mengikut Zon</h4></div>
-            <div class="panel-body">
-                <table class="table table-hover">
-                    <tr>
-                        <td>Perkara</td>
-                        <td>01</td>
-                        <td>02</td>
-                        <td>03</td>
-                        <td>04</td>
-                        <td>05</td>
-                        <td>06</td>
-                        <td>07</td>
-                        <td>08</td>
-                        <td>09</td>
-                        <td>10</td>
-                        <td>JUMLAH</td>
-                    </tr>
-                </table>
+            <div class="panel panel-primary">
+                <div class="panel panel-heading"><h4>Jadual Potongan Gaji Mengikut Zon</h4></div>
+                <div class="panel-body">
+                    <table class="table table-hover">
+                        <tr>
+                            <th>Perkara</th>
+                            <th>01</th>
+                            <th>02</th>
+                            <th>03</th>
+                            <th>04</th>
+                            <th>05</th>
+                            <th>06</th>
+                            <th>07</th>
+                            <th>08</th>
+                            <th>09</th>
+                            <th>10</th>
+                            <th>11</th>
+                            <th>12</th>
+                            <th>13</th>
+                            <th>14</th>
+                            <th>JUMLAH</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
