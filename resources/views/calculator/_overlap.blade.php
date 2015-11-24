27e104fac972aaf910161fbc076abd853ae6713e
unknown <suhairi81@gmail.com>
2015-11-24 11:31:16 +0800

    2015-11-24 1

--- resources/views/calculator/_overlap.blade.php
+++ resources/views/calculator/_overlap.blade.php
@@ -1,47 +1,45 @@
-<div class="col-xs-12">
-    <div class="panel panel-primary">
-    <div class="panel-heading"><h4>Maklumat Pinjaman Lepas</h4></div>
-    <div class="panel-body">
 
-        <?php $bil = 1; ?>
-        <table class="table table-striped table-hover">
-        <thead>
-        <tr>
-            <th>Bil</th>
-            <th>Jenis Pinjaman</th>
-            <th>Tarikh Pinjaman</th>
-            <th>Jumlah</th>
-            <th>Tempoh</th>
-            <th>Kadar</th>
-            <th>Caj Perkhidmatan</th>
-            <th>Insurans</th>
-            <th>Jumlah Keseluruhan</th>
-            <th>Baki</th>
-            <th>Status</th>
-        </tr>
-        </thead>
-        <tbody>
-        @forelse($akaunPotongan as $akaun)
-            <tr>
-                <td>{{ $bil++ }}</td>
-                <td>{{ $akaun->perkhidmatan->nama }}</td>
-                <td>{{ $akaun->created_at->format('d M Y') }}</td>
-                <td>{{ $akaun->jumlah }}</td>
-                <td>{{ $akaun-> tempoh }}</td>
-                <td>{{ $akaun->kadar }}</td>
-                <td>{{ $akaun->caj_perkhidmatan }}</td>
-                <td>{{ $akaun->insurans }}</td>
-                <td>{{ $akaun->jumlah_keseluruhan }}</td>
-                <td>{{ $akaun->baki }}</td>
-                <td>{{ $akaun->getStatus->nama }}</td>
-            </tr>
+<div class="row">
 
-        @empty
-            <tr><td colspan="11">Tiada Data.</td></tr>
+    <div class="col-xs-4">
+        <div class="panel panel-primary">
+            <div class="panel-heading"><h4>Overlap</h4></div>
+            <div class="panel-body">
 
-        @endforelse
-        </tbody>
-        </table>
+                <div class="form-group">
+                    <label for="jumlah">Jumlah Pinjaman</label>
+                    <input class="form-control" type="text"/>
+                </div>
 
+                <div class="form-group">
+                    <label for="tempoh">Tempoh</label>
+                    <input class="form-control" type="number"/>
+                </div>
+
+            </div>
+        </div>
     </div>
+
+    <div class="col-xs-4">
+        <div class="panel panel-primary">
+            <div class="panel-heading"><h4>Maklumat Pinjaman</h4></div>
+            <div class="panel-body">
+                <table class="table table-condensed table-striped">
+                    <tr>
+                        <th>Baki Pinjaman</th>
+                        <td align="right">RM 4200.00</td>
+                    </tr>
+                    <tr>
+                        <th>Baki Tempoh</th>
+                        <td align="right">60 bulan</td>
+                    </tr>
+                    <tr>
+                        <th>Jumlah Langsai</th>
+                        <td align="right">RM 3600.00</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+
 </div>
