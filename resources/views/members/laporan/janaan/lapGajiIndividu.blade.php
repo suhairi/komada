df878ce80798780a0ab927434f9a6898e8c899e3
suhairi81 <suhairi81@gmail.com>
2015-12-24 23:35:09 +0800

    2015-12-24 1

--- resources/views/members/laporan/janaan/lapGajiIndividu.blade.php
+++ resources/views/members/laporan/janaan/lapGajiIndividu.blade.php
@@ -3,56 +3,51 @@
 @section('content')
 
     <div class="row">
-        <div class="col-xs-12">
-
+        <div class="col-xs-6">
             <div class="panel panel-primary">
-                <div class="panel-heading"><h4>{{ $bahagian-> nama }} ({{ $bahagian->kod }})</h4></div>
+                <div class="panel panel-heading"><h4>Jadual Potongan Gaji Individu</h4></div>
                 <div class="panel-body">
 
-                    <table class="table table-hover table-bordered">
-                        <tr>
-                            <th>Bil</th>
-                            <th>No Pek</th>
-                            <th>Nama</th>
-                            <th>Yuran</th>
-                            <th>TKA</th>
-                            <th>PER</th>
-                            <th>M/P</th>
-                            <th>S/K</th>
-                            <th>W/T</th>
-                            <th>K/C</th>
-                            <th>B/S</th>
-                            <th>T/B</th>
-                            <th>Ins</th>
-                            <th><div align="right">JUMLAH</div></th>
-                        </tr>
-                        @foreach($persons as $person)
-                            <tr>
-                                <td>{{ $bil++ }}</td>
-                                <td>{{ $person['no_gaji'] }}</td>
-                                <td>{{ $person['nama'] }}</td>
-                                <td align="right">{{ number_format($person['yuran'], 2) }}</td>
-                                <td align="right">{{ number_format($person['tka'], 2) }}</td>
-                                <td align="right">{{ number_format($person['pertaruhan'], 2) }}</td>
-                                <td align="right">{{ number_format($person['takaful'], 2) }}</td>
-                                <td align="right">{{ number_format($person['sumbangan'], 2) }}</td>
-                                <td align="right">{{ number_format($person['pwt'], 2) }}</td>
-                                <td align="right">{{ number_format($person['kecemasan'], 2) }}</td>
-                                <td align="right">{{ number_format($person['bsekolah'], 2) }}</td>
-                                <td align="right">{{ number_format($person['tb'], 2) }}</td>
-                                <td align="right">{{ number_format($person['ins'], 2) }}</td>
-                                <th><div align="right">{{ number_format($person['jumlah'], 2) }}</div></th>
-                            </tr>
-                        @endforeach
-                        <tr>
-                            <td align="right" colspan="13"><strong>JUMLAH BESAR</strong></td>
-                            <th><div align="right">{{ number_format($jumlahBesar, 2) }}</div></th>
-                        </tr>
-                    </table>
+                    <form method="post" action="{{ route('members.laporan.lapGajiIndividu.generate') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group">
+                            <label for="zon">Zon PembayaranGaji</label>
+                            <select name="zon" class="form-control">
+                                <option>Zon Pembayaran Gaji</option>
+
+                                @foreach($zones as $zone)
+                                    <option value="{{ $zone->kod }}">{{ $zone->kod }} - {{ $zone->nama }}</option>
+                                @endforeach
+
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="bulan">Bulan / Tahun</label>
+                            <select name="bulan" class="form-control">
+                                <option>Bulan</option>
+                                @for($i=1; $i<=12; $i++)
+                                    <option value="{{$i}}">{{ $i }}</option>
+                                @endfor
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <select name="tahun" class="form-control">
+                                <option>Tahun</option>
+                                @for($i=2015; $i<=date('Y'); $i++)
+                                    <option value="{{$i}}">{{ $i }}</option>
+                                @endfor
+                            </select>
+                        </div>
+
+                        <div align="right">@include('buttons._submit', ['value' => 'Jana Laporan'])</div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
+
 
 
 @stop
