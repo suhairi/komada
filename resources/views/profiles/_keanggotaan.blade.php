274314eba34886f39d66b5141c878c74ce968394
unknown <suhairi81@gmail.com>
2016-01-18 16:36:45 +0800

    2016-01-18 1

--- resources/views/profiles/_keanggotaan.blade.php
+++ resources/views/profiles/_keanggotaan.blade.php
@@ -1,15 +1,13 @@
 <table class="table">
     <tr>
         <th>Jumlah Keseluruhan</th>
-        <th>Jumlah Aktif</th>
-        <th>Tidak Aktif Tahun Ini</th>
-        <th>Tidak Aktif Tahun Sebelum</th>
+        <th>Jumlah Tidak Aktif</th>
+        <th>Jumlah Aktif Semasa</th>
         <th>No Anggota Terakhir</th>
     </tr>
     <tr>
         <td>{{ $anggota['keseluruhan'] }}</td>
         <td>{{ $anggota['aktif'] }}</td>
-        <td>{{ $anggota['inactive'] }}</td>
         <td>{{ ($anggota['keseluruhan'] - $anggota['aktif'] -$anggota['inactive']) }}</td>
         <td>{{ $anggota['no_akhir'] }}</td>
     </tr>
